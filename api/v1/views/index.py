fa14a751b3c988344aa5a4c3ee2c4e84e8f597ae
Cesar Molina <cesaruxfxtx@gmail.com>
2022-05-30 15:48:28 -0500

    Fixing up task 4

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,7 +2,7 @@
 """Module with a Blueprint object for the API"""
 from flask import jsonify
 from models import storage
-from api.v1.views.base_views import app_views
+from api.v1.views import app_views
 
 
 @app_views.route('/status', strict_slashes=False)
